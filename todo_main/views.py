2a5a868662e756090ecb0096bf6a2e6e787319d4
Robert' RP <roberto.riveros.padilla101@gmail.com>
2024-04-10 21:32:38 -0600

    todo app final

--- todo_main/views.py
+++ todo_main/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from todos.models import Task
 
 def home(request):
-    tasks = Task.objects.filter(is_completed=False).order_by('-update_at')
+    incompleted_tasks = Task.objects.filter(is_completed=False).order_by('-update_at')
+    completed_tasks = Task.objects.filter(is_completed=True)
     context = {
-        'tasks' : tasks,
+        'incompleted_tasks' : incompleted_tasks,
+        'completed_tasks' : completed_tasks,
     }
-    print(tasks)
     return render(request,'index.html', context)
